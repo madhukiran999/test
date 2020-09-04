54508cb9c4a03882428a670af6b68508202a02f0
madhukiran999 <59006421+madhukiran999@users.noreply.github.com>
2020-09-04 10:40:12 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,5 @@
-import spark.servlet.SparkApplication;
-
-import static spark.Spark.get;
-
-public class HelloWorld implements SparkApplication {
-	public static void main(String[] args) {
-		new HelloWorld().init();
-	}
-
-	@Override
-	public void init() {
-		get("/hello", (req, res) -> "Hello World");
-	}
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
 }
